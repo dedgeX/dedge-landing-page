98c354545e59b5aeed9348ef8df5c0a8ab316065
grobelr <grobelr@gmail.com>
2024-09-13 22:45:09 -0300

    adjusts

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dedge-landing-page/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/dedge-landing-page' : '',
+  assetPrefix: '/dedge-landing-page/',
+  basePath: '/dedge-landing-page',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
