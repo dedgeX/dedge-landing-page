2f03b6f89b4fdaa2564085e1fa9a5fde28aaa375
grobelr <grobelr@users.noreply.github.com>
2024-09-14 03:32:21 +0000

    deploy: ab4399f8bfda877b0e95b4829290c84692cef539

---
+++ _next/static/dHqJggsPZ7m_jH6fxcgAO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/959-f7b3a728fceaa45a.js","static/chunks/pages/index-a4699ef409c39b39.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
