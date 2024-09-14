1b75f3be2859c36e92b224ed614c1803b5150f8c
grobelr <grobelr@gmail.com>
2024-09-13 21:39:34 -0300

    add to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/dedge-landing-page/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/dedge-landing-page' : '',
+};
 
 export default nextConfig;
