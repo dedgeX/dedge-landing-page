b2ebc4d829ed0e97fbe28e4ed7e1bf0dc2c73773
grobelr <grobelr@gmail.com>
2024-09-15 20:49:09 -0300

    djusts

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dedge-landing-page/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/dedge-landing-page' : '',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/dedge-landing-page/' : '',
+//   basePath: process.env.NODE_ENV === 'production' ? '/dedge-landing-page' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+
   images: {
     unoptimized: true,
   },
