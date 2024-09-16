45b0cdbdb4645b391deb903558ca181d12f49550
grobelr <grobelr@users.noreply.github.com>
2024-09-16 00:18:48 +0000

    deploy: 76126dedcac4deeb50ecc065a77803bc0eaf3286

---
+++ _next/static/MhI1BbboM5yuRuP3ELa7G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/959-47042e5dcb7d9879.js","static/chunks/pages/index-64053fc4daf1179b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
